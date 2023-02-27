a2fdb197c37a10f5214a2e82daccf54de8dbdeba
Johora-Juhi <zohrajuhi1012@gmail.com>
2023-02-27 22:29:20 +0600

    product quantity removed from cart

--- src/redux/reducers/ProductReducer/ProductReducer.js
+++ src/redux/reducers/ProductReducer/ProductReducer.js
@@ -5,26 +5,43 @@ const initialstate = {
 };
 
 const ProductReducer = (state = initialstate, action) => {
-  const addedProduct = state.cart.find(product => product._id === action.payload._id)
+  const addedProduct = state.cart.find(
+    (product) => product._id === action.payload._id
+  );
   switch (action.type) {
     case ADD_TO_CART:
-      if(addedProduct){
-        const newCart = state.cart.filter(product=> product._id !== action.payload._id)
+      if (addedProduct) {
+        const newCart = state.cart.filter(
+          (product) => product._id !== action.payload._id
+        );
 
-addedProduct.quantity = addedProduct.quantity + 1        
+        addedProduct.quantity = addedProduct.quantity + 1;
         return {
           ...state,
-          cart: [...newCart, addedProduct]
+          cart: [...newCart, addedProduct],
         };
       }
       return {
         ...state,
-        cart: [...state.cart, {...action.payload, quantity: 1}],
+        cart: [...state.cart, { ...action.payload, quantity: 1 }],
       };
     case REMOVE_FROM_CART:
+      if (addedProduct.quantity > 1) {
+        const newCart = state.cart.filter(
+          (product) => product._id !== action.payload._id
+        );
+
+        addedProduct.quantity = addedProduct.quantity - 1;
+        return {
+          ...state,
+          cart: [...newCart, addedProduct],
+        };
+      }
       return {
         ...state,
-        cart: state.cart.filter(product => product._id !== action.payload._id)
+        cart: state.cart.filter(
+          (product) => product._id !== action.payload._id
+        ),
       };
     default:
       return state;
